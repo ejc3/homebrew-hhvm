a554fee03f32ad8a83b9ecc83cbbf556b2d45d0f
EJ <ejc3@yahoo-inc.com>
2016-10-10 12:26:14 -0700

    Update hhvm.rb
    adding fix for >10.11 and xcode 8 clock_gettime

--- hhvm.rb
+++ hhvm.rb
@@ -94,6 +94,12 @@ class Hhvm < Formula
     cmake_args << "-DCMAKE_C_FLAGS=-I#{Formula["readline"].opt_include} -L#{Formula["readline"].opt_lib}"
     cmake_args << "-DCMAKE_CXX_FLAGS=-I#{Formula["readline"].opt_include} -L#{Formula["readline"].opt_lib}"
 
+    # https://github.com/hhvm/homebrew-hhvm/issues/58 (works around a change in Mac OS X 10.11
+    # from here: https://github.com/Homebrew/homebrew-core/issues/2674#issuecomment-250896730
+    if MacOS.version >= "10.11" && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
+      cmake_args << "-DHAVE_CLOCK_GETTIME:INTERNAL=0"
+    end
+
     # Dependency information.
     cmake_args += %W[
       -DAWK_EXECUTABLE=#{Formula["gawk"].opt_bin}/gawk
